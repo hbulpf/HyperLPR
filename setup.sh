45be18dfa09c69883aef714d7dadf6e9a8606086
icepointCLY <1023553676@qq.com>
2018-07-29 10:08:02 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install pillow
+sudo pip install scikit-image
+sudo pip install opencv-python
+sudo pip install argparse
+sudo pip install tensorflow
+sudo pip install keras
