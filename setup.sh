44b35250f57016392b13aae8c4097a7e64bb281a
icepointCLY <1023553676@qq.com>
2018-08-04 18:57:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 sudo apt-get install python
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
-sudo pip install pillow
-sudo pip install scikit-image
-sudo pip install opencv-python
-sudo pip install argparse
-sudo pip install tensorflow
-sudo pip install keras
+
+sudo pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+sudo apt-get install python-numpy -y
+sudo apt-get install python-scipy -y
+sudo apt-get install python-matplotlib -y
+sudo pip install scikit-image -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+sudo pip install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple
+sudo pip install tensorflow -i https://pypi.tuna.tsinghua.edu.cn/simple
+sudo pip install keras -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+git clone https://github.com/icepoint666/HyperLPR.git
