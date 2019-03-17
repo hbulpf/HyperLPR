12e9481bf1c881d195bf53242ce28a9117087e28
LPF-OFFICE <hudalpf@163.com>
2019-03-17 15:27:37 +0800

    去掉sudo

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-sudo apt-get install python
+apt-get install python
 wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
+python get-pip.py
 
-sudo pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-sudo apt-get install libopencv-dev -y
-sudo apt-get install python-numpy -y
-sudo apt-get install python-scipy -y
-sudo apt-get install python-matplotlib -y
-sudo pip install scikit-image -i https://pypi.tuna.tsinghua.edu.cn/simple
+apt-get install libopencv-dev -y
+apt-get install python-numpy -y
+apt-get install python-scipy -y
+apt-get install python-matplotlib -y
+pip install scikit-image -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-sudo pip install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple
-sudo pip install tensorflow -i https://pypi.tuna.tsinghua.edu.cn/simple
-sudo pip install keras -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install tensorflow -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install keras -i https://pypi.tuna.tsinghua.edu.cn/simple
 
